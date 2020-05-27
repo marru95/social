33846261945e5090fca0353584970990c92abd44
vagrant <vagrant@homestead>
2020-05-27 03:45:14 +0000

    Final video 16

--- resources/assets/js/components/StatusesList.vue
+++ resources/assets/js/components/StatusesList.vue
@@ -1,7 +1,17 @@
 <template>
     <div>
-        <div v-for="status in statuses" v-text="status.body"></div>
-
+        <div class="card border-0 mb-3 shadow" v-for="status in statuses">
+            <div class="card-body d-flex flex-column">
+                <div class=" d-flex align-items-center mb-3">
+                <img class="rounded mr-3 shadow" width="40px" src="Imagen-2.jpg" alt="">
+                    <div class="">
+                        <h5 class="mb-1">Marruan Adib</h5>
+                        <div class="small text-muted" >Hace una hora</div>
+                    </div>
+                </div>
+                <p class="card-text text-secondary" v-text="status.body"></p>
+            </div>
+        </div>
     </div>
 </template>
 
