65d0443e2891ebccef09e9597d59b77a78608602
vagrant <vagrant@homestead>
2020-06-05 13:48:21 +0000

    Final video 33

--- public/js/app.js
+++ public/js/app.js
@@ -50117,7 +50117,7 @@ exports = module.exports = __webpack_require__(2)(false);
 
 
 // module
-exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", ""]);
+exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", ""]);
 
 // exports
 
@@ -50130,6 +50130,30 @@ exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__LikeBtn__ = __webpack_require__(56);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__LikeBtn___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__LikeBtn__);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 //
 //
 //
@@ -50447,61 +50471,116 @@ var render = function() {
           _c("span", { attrs: { dusk: "likes-count" } }, [
             _vm._v(_vm._s(_vm.status.likes_count))
           ])
-        ]),
-        _vm._v(" "),
-        _c(
-          "form",
-          {
-            on: {
-              submit: function($event) {
-                $event.preventDefault()
-                return _vm.addComment($event)
-              }
-            }
-          },
-          [
-            _c("textarea", {
-              directives: [
-                {
-                  name: "model",
-                  rawName: "v-model",
-                  value: _vm.newComment,
-                  expression: "newComment"
-                }
-              ],
-              attrs: { name: "comment" },
-              domProps: { value: _vm.newComment },
-              on: {
-                input: function($event) {
-                  if ($event.target.composing) {
-                    return
-                  }
-                  _vm.newComment = $event.target.value
-                }
+        ])
+      ],
+      1
+    ),
+    _vm._v(" "),
+    _c(
+      "div",
+      { staticClass: "card-footer" },
+      [
+        _vm._l(_vm.comments, function(comment) {
+          return _c("div", { staticClass: "mb-3" }, [
+            _c("img", {
+              staticClass: "rounded shadow-sm float-left mr-2",
+              attrs: {
+                width: "34px",
+                src: comment.user_avatar,
+                alt: comment.user_name
               }
             }),
             _vm._v(" "),
-            _c("button", { attrs: { dusk: "comment-btn" } }, [_vm._v("Enviar")])
-          ]
-        ),
-        _vm._v(" "),
-        _vm._l(_vm.comments, function(comment) {
-          return _c("div", [
-            _vm._v(
-              "\n            " +
-                _vm._s(comment.user_name) +
-                "\n            " +
-                _vm._s(comment.body) +
-                "\n        "
-            )
+            _c("div", { staticClass: "card border-0 shadow-sm" }, [
+              _c("div", { staticClass: "card-body p-2 text-secondary" }, [
+                _c("a", { attrs: { href: "@#" } }, [
+                  _c("strong", [_vm._v(_vm._s(comment.user_name))])
+                ]),
+                _vm._v(
+                  "\n                        " +
+                    _vm._s(comment.body) +
+                    "\n                    "
+                )
+              ])
+            ])
           ])
-        })
+        }),
+        _vm._v(" "),
+        _vm.isAuthenticated
+          ? _c(
+              "form",
+              {
+                on: {
+                  submit: function($event) {
+                    $event.preventDefault()
+                    return _vm.addComment($event)
+                  }
+                }
+              },
+              [
+                _c("div", { staticClass: "d-flex align-items-center" }, [
+                  _c("img", {
+                    staticClass: "rounded shadow-sm  mr-2",
+                    attrs: {
+                      width: "34px",
+                      src:
+                        "https://image.shutterstock.com/image-vector/people-icon-vector-user-symbol-600w-1714434235.jpg",
+                      alt: _vm.currentUser.name
+                    }
+                  }),
+                  _vm._v(" "),
+                  _c("div", { staticClass: "input-group" }, [
+                    _c("textarea", {
+                      directives: [
+                        {
+                          name: "model",
+                          rawName: "v-model",
+                          value: _vm.newComment,
+                          expression: "newComment"
+                        }
+                      ],
+                      staticClass: "form-control border-0 shadow-sm",
+                      attrs: {
+                        name: "comment",
+                        placeholder: "Escribe un comentario...",
+                        rows: "1"
+                      },
+                      domProps: { value: _vm.newComment },
+                      on: {
+                        input: function($event) {
+                          if ($event.target.composing) {
+                            return
+                          }
+                          _vm.newComment = $event.target.value
+                        }
+                      }
+                    }),
+                    _vm._v(" "),
+                    _vm._m(0)
+                  ])
+                ])
+              ]
+            )
+          : _vm._e()
       ],
       2
     )
   ])
 }
-var staticRenderFns = []
+var staticRenderFns = [
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { staticClass: "input-group-append" }, [
+      _c(
+        "button",
+        { staticClass: "btn btn-primary", attrs: { dusk: "comment-btn" } },
+        [_vm._v("Enviar")]
+      )
+    ])
+  }
+]
 render._withStripped = true
 module.exports = { render: render, staticRenderFns: staticRenderFns }
 if (false) {
--- resources/assets/js/components/StatusListItem.vue
+++ resources/assets/js/components/StatusListItem.vue
@@ -11,22 +11,46 @@
             <p class="card-text text-secondary" v-text="status.body"></p>
         </div>
         <div class="card-footer p-2 d-flex justify-content-between align-items-center">
+
                 <like-btn
                     :status="status"
                 ></like-btn>
+
             <div class="text-secondary mr-2">
                 <i class="far fa-thumbs-up"></i>
                 <span dusk="likes-count">{{ status.likes_count}}</span>
             </div>
-            <form @submit.prevent="addComment">
-                <textarea name="comment" v-model="newComment"></textarea>
-                <button dusk="comment-btn">Enviar</button>
-            </form>
-            <div v-for="comment in comments">
-                {{ comment.user_name }}
-                {{ comment.body }}
-            </div>
+
         </div>
+            <div class="card-footer">
+
+                <div v-for="comment in comments" class="mb-3">
+                    <img class="rounded shadow-sm float-left mr-2" width="34px" :src="comment.user_avatar" :alt="comment.user_name">
+                    <div class="card border-0 shadow-sm">
+                        <div class="card-body p-2 text-secondary">
+                            <a href="@#"><strong>{{ comment.user_name }}</strong></a>
+                            {{ comment.body }}
+                        </div>
+                    </div>
+                </div>
+                <form @submit.prevent="addComment" v-if="isAuthenticated">
+                    <div class="d-flex align-items-center">
+                        <img class="rounded shadow-sm  mr-2" width="34px"
+                             src="https://image.shutterstock.com/image-vector/people-icon-vector-user-symbol-600w-1714434235.jpg"
+                             :alt="currentUser.name">
+                        <div class="input-group">
+                            <textarea class="form-control border-0 shadow-sm"
+                                      name="comment"
+                                      v-model="newComment"
+                                      placeholder="Escribe un comentario..."
+                                      rows="1"></textarea>
+                            <div class="input-group-append">
+                             <button class="btn btn-primary" dusk="comment-btn">Enviar</button>
+                            </div>
+                            </div>
+                    </div>
+                </form>
+            </div>
     </div>
 </template>
 
